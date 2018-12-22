9a9d40d8934d500aec393a53c077b502273175db
Naoki Wakisaka <naoki.wakisaka@ams.eng.osaka-u.ac.jp>
2018-12-23 04:05:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@
 #include <vector>
 #include <utility>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 
 template<class T>
-class Div1 : public Solver<T> {
+class Div : public Solver<T> {
 public:
 	void solve(T num){
 		for( T n=num, p=2; n > 1; p++ ){
@@ -20,6 +21,59 @@ public:
 			this->result.push_back(make_pair(p, cnt));
 		}
 	}
+};
+
+template<class T>
+class DivSqrt : public Solver<T> {
+public:
+	void solve(T num){
+		for( T n=num, p=2; n > 1 && p < static_cast<T>(sqrt(num))+1; p++ ){
+			T cnt = 0;
+			for( ; n%p == 0; cnt++ ){
+				n /= p;
+			}
+			if( cnt == 0 ) continue;
+			this->result.push_back(make_pair(p, cnt));
+		}
+		if( this->result.size() == 0 ){
+			this->result.push_back(make_pair(num, 1));
+		}
+	}
+};
+
+template<class T>
+class DivPrim : public Solver<T> {
+public:
+	void solve(T num){
+		T snum = static_cast<T>(sqrt(num))+1;
+		createPrimList(snum);
+		T n = num;
+		for( T n=num, p=2; n > 1 && p < snum; p++ ){
+			if( !plist[p] ) continue;
+			T cnt = 0;
+			for( ; n%p == 0; cnt++ ){
+				n /= p;
+			}
+			if( cnt == 0 ) continue;
+			this->result.push_back(make_pair(p, cnt));
+		}
+		if( this->result.size() == 0 ){
+			this->result.push_back(make_pair(num, 1));
+		}
+	}
+
+private:
+	void createPrimList(T num){
+		plist.resize(num, true);
+		for( T i=4; i<num; i+=2 ) plist[i] = false;
+		for( T i=3; i<num; i+=2 ){
+			for( T j=i; j*i <= num; j++ ){
+				plist[j*i] =  false;
+			}
+		}
+	}
+
+	vector<bool> plist;
 };
 
 typedef long long type;
@@ -35,7 +89,9 @@ int main(int argc, char *argv[])
 	vector<type> list = {12345,  1000000007, 5368709120, 12345678987654321};
 	for( auto &val : list ){
 		cout << "num: " << val << endl;
-		output( Div1<type>(), val );
+		output( Div<type>(), val );
+		output( DivSqrt<type>(), val );
+		output( DivPrim<type>(), val );
 		cout << endl;
 	}
 
